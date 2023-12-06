199bbdc8b3659a8ccfcb558325937bb227f41afd
thamindu <IT18177688@my.sliit.lk>
2023-12-06 13:27:09 +0530

    [INITAIL-COMMIT] Stater Lessons

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Sample Site</title>
+    <style>
+      
+      body {
+        display: grid;
+        place-items: center;
+        height: 100vh;
+        margin: 0;
+        font-family: sans-serif;
+        background-color: black;
+        color: white;
+      }
+
+    </style>
+  </head>
+  <body>
+
+    <h1> Recommended Books </h1>
+
+    <?php
+    $books = [
+      [
+      'name' => 'Do Androids Dream of Electric Sheep',
+      'author' => 'Philip K Dick',
+      'Website' => 'http://example.com',
+      'releasedYear' => 2015
+      ],
+      [
+      'name' => 'The Lanolins',
+      'author' => 'Andy Weir',
+      'Website' => 'http://example.com',
+      'releasedYear' => 2016
+      ]
+    ];
+    ?>
+
+    <ul>
+      <?php foreach ($books as $book) : ?>
+        <li> 
+          <a href="<?= $book['Website']?>"; >
+            <?= $book['name']; ?> 
+          </a>
+        </li>
+      <?php endforeach; ?>
+    </ul>
+
+  </body>
+</html>
