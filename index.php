7bd0e3534f162824756479a41fd983f803ba262a
thamindu <IT18177688@my.sliit.lk>
2024-01-10 13:29:23 +0530

    [added] db connection and tested

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
 
 require "functions.php";
-require "router.php";
+// require "router.php";
+
+// connect to our database
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;password=admin;charset=utf8mb4;";
+$pdo = new PDO($dsn);
+$statement = $pdo->prepare("SELECT * FROM posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
