1f3ffa9f8c9c3a3bce3c252db704ce0bb99e2c67
thamindu <IT18177688@my.sliit.lk>
2024-01-18 11:20:27 +0530

    [fix] fixed sql injection vulnerabilities

--- index.php
+++ index.php
@@ -5,10 +5,15 @@ require "Database.php";
 // require "router.php";
 
 $config = require('config.php');
-
 $db = new Database($config['database']);
-$posts = $db->query("SELECT * FROM posts")->fetchAll();
 
-foreach ($posts as $post) {
-    echo "<li>" . $post['title'] . "</li>";
-}
+$id = $_GET['id'];
+$query = "SELECT * FROM posts WHERE id = :id"; // :id wildcard
+
+$posts = $db->query($query, [':id' => $id])->fetch();
+
+var_dump($posts);
+
+// foreach ($posts as $post) {
+//     echo "<li>" . $post['title'] . "</li>";
+// }
