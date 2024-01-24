31857134485b12e4d7f40caa297b140045939279
thamindu <IT18177688@my.sliit.lk>
2024-01-24 22:53:14 +0530

    [fix] refactor database class and note

--- controllers/notes.php
+++ controllers/notes.php
@@ -6,6 +6,6 @@ $db = new Database($config['database']);
 
 $header = "My Notes";
 
-$notes = $db->query('select * from notes where user_id = 1')->fetchAll();
+$notes = $db->query('select * from notes where user_id = 1')->findAll();
 
 require "views/notes.view.php";
--- index.php
+++ index.php
@@ -4,14 +4,3 @@ require "functions.php";
 require "Database.php";
 require "Response.php";
 require "router.php";
-
-// $id = $_GET['id'];
-// $query = "SELECT * FROM posts WHERE id = :id"; // :id wildcard
-
-// $posts = $db->query($query, [':id' => $id])->fetch();
-
-// var_dump($posts);
-
-// foreach ($posts as $post) {
-//     echo "<li>" . $post['title'] . "</li>";
-// }
