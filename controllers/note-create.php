c7d7b532a01bf78007d6f1e9d3ffb50e59c2ac39
thamindu <IT18177688@my.sliit.lk>
2024-01-25 11:29:25 +0530

    [fix] finally persist a new note to the database. But, in doing so, you'll be introduced to a new security concern that requires us to always escape user-provided input.

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,9 +1,16 @@
 <?php
 
+
+$config = require('config.php');
+$db = new Database($config['database']);
+
 $header = "Create Note";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    dd($_POST);
+    $db -> query('INSERT INTO notes(body,user_id) VALUES(:body, :user_id)', [
+        'body' => $_POST['body'],
+        'user_id' => 1,
+    ]);
 }
 
 require "views/note-create.view.php";
