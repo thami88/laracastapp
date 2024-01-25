f746897ea6215ad193217f736c43bc04a48084f8
thamindu <IT18177688@my.sliit.lk>
2024-01-25 12:06:16 +0530

    [adding] client side and server side validations to the form

--- views/note-create.view.php
+++ views/note-create.view.php
@@ -17,7 +17,18 @@
             <div class="col-span-full">
             <label for="body" class="block text-sm font-medium leading-6 text-gray-900">Body</label>
             <div class="mt-2">
-                <textarea id="body" name="body" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                <textarea 
+                id="body" 
+                name="body" 
+                rows="3" 
+                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                required
+                >
+                <?= $_POST['body'] ?? '' ?>
+            </textarea>
+                <?php if (isset($errors['body'])) : ?>
+                    <p class="text-red-500 text-xs mt-2"><?= $errors['body'] ?></p>
+                <?php endif; ?>
             </div>
             <p class="mt-3 text-sm leading-6 text-gray-600">Here's an idea for a new note....</p>
             </div>
