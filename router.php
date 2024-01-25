08b1aa1994f7acb1c068e2faf92333bdfbdc5c0d
thamindu <IT18177688@my.sliit.lk>
2024-01-25 10:47:44 +0530

    [adding] added form to create notes, added note create router and styles

--- router.php
+++ router.php
@@ -1,14 +1,6 @@
 <?php
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/notes' => 'controllers/notes.php',
-    '/note' => 'controllers/note.php',
-    '/contact' => 'controllers/contact.php',
-];
+$routes = require "routes.php";
 
 function routeToController($uri, $routes)
 {
@@ -26,5 +18,6 @@ function abort($code = 404)
     die();
 }
 
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 routeToController($uri, $routes);
--- views/notes.view.php
+++ views/notes.view.php
@@ -6,13 +6,21 @@
   
   <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-      <?php foreach ($notes as $note) : ?>
-        <li>
-          <a href="/note?id=<?=$note['id'] ?>" class="text-blue-500 hover:underline">
-            <?= $note['body']?>
+      <ul>
+        <?php foreach ($notes as $note) : ?>
+          <li>
+            <a href="/note?id=<?=$note['id'] ?>" class="text-blue-500 hover:underline">
+              <?= $note['body']?>
+            </a>
+          </li>
+          <?php endforeach; ?>
+        </ul>
+
+        <p class="mt-6">
+          <a href="/notes/create" class="text-blue-500 underline">
+            create note
           </a>
-        </li>
-      <?php endforeach; ?>
+        </p>
     </div>
   </main>
 
