407a60fad4589e47bbe4a1f293848f6f0a94a022
thami88 <thaminduliyanage97@gmail.com>
2024-04-09 21:34:06 +0530

    [fix] delete create not working issue is fixed

--- Core/Database.php
+++ Core/Database.php
@@ -9,7 +9,7 @@ class Database
     public $connection;
     public $statement;
 
-    public function __construct($config, $username = 'root', $password = 'admin')
+    public function __construct($config, $username = 'root', $password = '')
     {
         $dsn = 'mysql:' . http_build_query($config, '', ';');
         $this->connection = new PDO($dsn, $username, $password, [
