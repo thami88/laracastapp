82a9d0acb2db112ccd5eed49507910d73197099f
thami88 <thaminduliyanage97@gmail.com>
2024-05-16 07:19:05 +0530

    [adding] Password hashing

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -39,7 +39,7 @@ if ($user) {
     // if not, save one to the database, and then log the user in, and redirect
     $db->query('INSERT INTO users (email,password) VALUES (:email, :password)',[
         'email' => $email,
-        'password' => $password
+        'password' => password_hash($password, PASSWORD_DEFAULT)
     ]);
 
     // mark that user logged in
