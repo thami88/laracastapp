0619a549f432b25693faeff0986f3a7022ab165c
thami88 <thaminduliyanage97@gmail.com>
2024-05-18 09:38:48 +0530

    [refactor] clean up our controller and improve clarity by extracting an Authenticator class

--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -1,44 +1,22 @@
 <?php
 
-use Core\App;
-use Core\Database;
-use Core\Validator;
+use Core\Authenticator;
 use Http\Forms\LoginForm;
-
-$db = App::resolve(Database ::class);
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
 $form = new LoginForm();
 
-if(! $form->validate($email,$password)){
-  
-    return view('session/create.view.php', [
-    'errors' =>  $form->errors()
-    ]); 
-}
+if($form->validate($email,$password)){
 
-$user = $db->query('select * from users where email = :email',[
-    'email' => $email
-])->find();
-
-if ($user) {
-    if (password_verify($password,$user['password'])) {
-        login([
-            'email' => $email
-        ]);   
-    
-        header('location: /');
-        exit(); 
+    if((new Authenticator)->attempt($email, $password)){
+        redirect('/');
     }
+    
+    $form->error('email','No matching account found for that email address and password');
 }
 
 return view('session/create.view.php', [
-    'errors' =>  [
-        'email' => 'No matching account found for that email address and password'
-    ]
-]); 
-
-
- 
+    'errors' =>  $form->errors()
+]);
